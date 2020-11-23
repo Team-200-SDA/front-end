ec825818d1bf94fc91eb5aed5eab99f3e74bbeaf
pXius <fan.engelbrecht@gmail.com>
2020-11-23 16:45:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Import custom styles for our application
+import './css/App.css';
+
+import Auth from './services/Auth';
+import Navbar from './components/layout/Navbar';
+
+// Import pages
+import LoginPage from './components/auth/LoginPage';
+import HomePage from './components/home/HomePage';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(Auth.isLoggedIn());
+  Auth.bindLoggedInStateSetter(setLoggedIn);
+
+  const loggedInRouter = (
+    <Router>
+      <Navbar onLogout={() => Auth.logout()} />
+
+      <div className="container mt-5">
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+
+  return loggedIn ? loggedInRouter : <LoginPage />;
+}
+
+export default App;
