348f5efb8a3bc697ba43028f23ef7181adeb28d5
Radha <radhasr1986@gmail.com>
2020-11-27 15:36:41 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Auth from './services/Auth';
-import Navbar from './components/layout/Navbar';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Auth from "./services/Auth";
+import Navbar from "./components/layout/Navbar";
 // Chat Bot
-import Chatbot from 'react-chatbot-kit';
-import ActionProvider from './chatbot-resources/ActionProvider';
-import MessageParser from './chatbot-resources/MessageParser';
-import config from './chatbot-resources/config';
- */// Import pages
-import LoginPage from './components/auth/LoginPage';
-import HomePage from './components/home/HomePage';
-import Chat from './components/chat/Chat';
-import LiveVideo from './components/live/LiveVideo/LiveVideo';
+import Chatbot from "react-chatbot-kit";
+import ActionProvider from "./chatbot-resources/ActionProvider";
+import MessageParser from "./chatbot-resources/MessageParser";
+import config from "./chatbot-resources/config";
+/// Import pages
+import LoginPage from "./components/auth/LoginPage";
+import HomePage from "./components/home/HomePage";
+import Chat from "./components/chat/Chat";
+import LiveVideo from "./components/live/LiveVideo/LiveVideo";
 // import Userprofile from './components/userprofile/UserProfile';
-import Lecture from './components/lecture/Lecture';
+import Lecture from "./components/lecture/Lecture";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(Auth.isLoggedIn());
