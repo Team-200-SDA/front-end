a037bd94bba47c9e754add49359f5fb742c8f5c9
Sneha <sneha.gupta@gmail.com>
2020-11-27 11:20:35 +0100

    Basic functional lecture page with basic styling

--- src/components/lecture/Lecture.jsx
+++ src/components/lecture/Lecture.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
-import LecturePage from './LecturePage';
+import React from 'react';
+import '../../css/lecture/lecture.css';
 
 export default function Lecture({lecture, deleteLecture}) {
-    const [bodyIsOpen, setBodyIsOpen] = useState(false);
-    const handleOpenMessage = () => {
-        setBodyIsOpen(!bodyIsOpen);
-    };
-
 
     return (
-        <div className="card mt-4">
-            <div className="card-title bg-secondary text-white m-0 p-1">
-                <div className="mw-75" 
-                    onClick={handleOpenMessage} 
-                    style={{ cursor: "pointer" }}>
-                    {lecture.title}
-                </div>
-            </div>
+        
+        <div className="card">
             <div className="card-body">
-                {lecture.link}
-            </div>
-            <div className="text-right">
-                <button className="btn btn-light mr-s"
-                onClick={() => deleteLecture(lecture.id)}>
-                Delete
+                <span>
+                    <a href= {lecture.link}
+                    className=""
+                    target ="_blank">{lecture.title}</a>
+                </span>
+                <button className="btn btn-light"
+                    onClick={() => deleteLecture(lecture.id)}>
+                    Delete<i class="fas fa-trash"></i>
                 </button>
             </div>
         </div>
-    );
+       
+        );
 
 }
