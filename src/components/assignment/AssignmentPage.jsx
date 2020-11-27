46c1f73cab9f556cd4166d71f43a6f04f055bb93
Sneha <sneha.gupta@gmail.com>
2020-11-27 19:09:51 +0100

    Add toggle between assignment and submission of the assignment

--- src/components/assignment/AssignmentPage.jsx
+++ src/components/assignment/AssignmentPage.jsx
@@ -12,6 +12,19 @@ import '../../css/assignment/assignmentPage.css';
 export default function AssignmentPage() {
     const [ assignments, setAssignments ] = useState([]);
 
+    const [ assignmentAssignedOn, setAssignmentAssignedOn ] = useState(true);
+    const [ assignmentSubmittedOn, setAssignmentSubmittedOn ] = useState(false);
+
+    const handleAssignAssignment = () => {
+        setAssignmentAssignedOn(!assignmentAssignedOn)
+        setAssignmentSubmittedOn(false)
+    }
+    const handleSubmitAssignment = () => {
+        setAssignmentAssignedOn(false)
+        setAssignmentSubmittedOn(!assignmentSubmittedOn)
+    }
+
+
     function getAllAssignments() {
         AssignmentApi.getAllAssignments()
             .then((data) => {
@@ -32,14 +45,40 @@ export default function AssignmentPage() {
     }
 
     return (
-        <div className= "assignment-div">
-            <CreateAssignment assignments={assignments} getAllAssignments={getAllAssignments}/>
 
-            { assignments.length === 0 ? "No assignment yet." : 
-                   assignments
-                    .map((assignment) => 
-                    <Assignment key={uuid()} assignment={assignment} deleteAssignment={deleteAssignment} />
-            )}
+        <div className= "assignment-page">
+            <div class="container d-flex justify-content-around mb-4 btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked onClick={handleAssignAssignment}/> Assigned Assignments
+                </label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off" onClick={handleSubmitAssignment} /> Submitted Assignments
+                </label>
+            </div>
+
+
+            { assignmentAssignedOn && 
+            <div className= "assignment-div">
+                <CreateAssignment assignments={assignments} getAllAssignments={getAllAssignments}/>
+
+                { assignments.length === 0 ? "No assignment yet." : 
+                    assignments
+                        .map((assignment) => 
+                        <Assignment key={uuid()} assignment={assignment} deleteAssignment={deleteAssignment} />
+                )}
+            </div>
+            }
+            { assignmentSubmittedOn && 
+            <div className= "assignment-div">
+                <CreateAssignment assignments={assignments} getAllAssignments={getAllAssignments}/>
+
+                { assignments.length === 0 ? "No assignment yet." : 
+                    assignments
+                        .map((assignment) => 
+                        <Assignment key={uuid()} assignment={assignment} deleteAssignment={deleteAssignment} />
+                )}
+            </div>
+            }
         </div>
     );
 
