7c4a2de7f2b252333a69739b6a9e0be9c97712a6
Sneha <sneha.gupta@gmail.com>
2020-11-30 17:56:28 +0100

    modify changes related to user role

--- src/components/lecture/Lecture.jsx
+++ src/components/lecture/Lecture.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../css/lecture/lecture.css';
 
-export default function Lecture({lecture, deleteLecture}) {
+export default function Lecture({lecture, deleteLecture, user_role}) {
 
     return (
         
@@ -12,10 +12,12 @@ export default function Lecture({lecture, deleteLecture}) {
                     className=""
                     target ="_blank">{lecture.title}</a>
                 </span>
+                {  user_role !== "teacher" ? null : 
                 <button className="btn btn-light"
-                    onClick={() => deleteLecture(lecture.id)}>
-                    Delete<i class="fas fa-trash"></i>
-                </button>
+                onClick={() => deleteLecture(lecture.id)}>
+                Delete
+            </button>
+            }
             </div>
         </div>
        
