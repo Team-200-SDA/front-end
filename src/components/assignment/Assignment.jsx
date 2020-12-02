d5a33270af81468bf30b78245b501442558ca9bf
Sneha <sneha.gupta@gmail.com>
2020-12-02 22:29:43 +0100

    Add student name to submission assignment for teacher

--- src/components/assignment/Assignment.jsx
+++ src/components/assignment/Assignment.jsx
@@ -5,11 +5,15 @@ import '../../css/assignment/assignment.css';
 export default function Assignment({ user, assignment, deleteAssignment}) {
     
     const user_ = window.sessionStorage.getItem('user');
-
+    console.log(assignment);
     return (
         
         <div className="card assignment-card">
             <div className="card-body">
+            {user_ !== assignment.user.name && assignment.user.role !== "teacher"
+                ?(<span className="user-name">
+                    {assignment.user.name}:</span>
+                ): null}
                 <span>
                     <Link to= {assignment.link}
                     className="assignment-link"
@@ -18,7 +22,7 @@ export default function Assignment({ user, assignment, deleteAssignment}) {
                 {user_ === assignment.user.name ?
                 (<button className="btn btn-light"
                     onClick={() => deleteAssignment(assignment.id)}>
-                    Delete
+                    <i className="fas fa-trash"></i>
                  </button>)
                 : null }
                 
