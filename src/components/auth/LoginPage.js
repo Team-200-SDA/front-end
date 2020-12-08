6c3b4b6464aa424c552f904d8f107e0f56f2fbb5
Radha <radhasr1986@gmail.com>
2020-12-08 12:25:39 +0100

    background image for login page added

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -4,44 +4,36 @@ import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
 function LoginPage() {
-    const login = async (loginData) => {
-        const loginSuccess = await Auth.login(loginData);
-        if (!loginSuccess) {
-            alert("Invalid credentials");
-        }
+  const login = async (loginData) => {
+    const loginSuccess = await Auth.login(loginData);
+    if (!loginSuccess) {
+      alert("Invalid credentials");
     }
+  };
 
-    const register = async (registrationData) => {
-        const registerSuccess = await Auth.register(registrationData);
-        if (!registerSuccess) {
-            alert("Couldn't register check credentials and try again");
-        }
+  const register = async (registrationData) => {
+    const registerSuccess = await Auth.register(registrationData);
+    if (!registerSuccess) {
+      alert("Couldn't register check credentials and try again");
     }
+  };
 
-    return (
-        <div className="wrapper">
-            <div className="container">
-                <div className="row mt-4">
-                    <div className="col-md-6 " style={{ color: "white" }}>
-                        <h1>SDA</h1>
-                        <p>Starter template</p>
-                    </div>
+  return (
+    <div className="login-page">
+      {/* <div className="container">
+          <div className="col-md-6"> */}
+            <div className="row">
+              <div className="col  strong-shadow">
+                <LoginForm onSubmit={login} />
+              </div>
 
-                    <div className="col-md-6">
-                        <div className="row">
-                            <div className="col-12  strong-shadow">
-                                <LoginForm onSubmit={login} />
-                            </div>
-
-                            <div className="col-12 mt-4">
-                                <RegisterForm onSubmit={register} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="col mt-4">
+                <RegisterForm onSubmit={register} />
+              </div>
             </div>
-        </div>
-    );
+          
+    </div>
+  );
 }
 
 export default LoginPage;
