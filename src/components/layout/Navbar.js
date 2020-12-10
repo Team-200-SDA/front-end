230ce914ecee4aea444f70b8d637884390cac7a9
pXius <fan.engelbrecht@gmail.com>
2020-12-10 19:13:34 +0100

    nav bar styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Calendar from '../calendar/Calendar';
-import Postspage from '../forum/posts/PostsPage'
 import { useContext } from 'react';
 import { LangContext } from '../../contexts/LanguageContext';
 
@@ -9,79 +8,84 @@ const logo = '/images/logo/logo.png';
 function Navbar({ onLogout }) {
   const { language, changeEn, changeSv } = useContext(LangContext);
   return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-brand">
-        <img src={logo} alt="" />
-      </Link>
+    <div className="nav-wrap">
+      <div className="navbar">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="" />
+        </Link>
 
-      <ul className="nav-links">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            {language.home}
-          </Link>
-        </li>
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">
+              {language.home}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/lectures" className="nav-link">
-            {language.Lectures}
-          </Link>
-        </li>
+          <li>
+            <Link to="/lectures" className="nav-link">
+              {language.Lectures}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/live" className="nav-link">
-            {language.Live_Stream}
-          </Link>
-        </li>
+          <li>
+            <Link to="/live" className="nav-link">
+              {language.Live_Stream}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/assignments" className="nav-link">
-            {language.Assignments}
-          </Link>
-        </li>
+          <li>
+            <Link to="/assignments" className="nav-link">
+              {language.Assignments}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/filestorage" className="nav-link">
-            File Storage
-          </Link>
-        </li>
+          <li>
+            <Link to="/filestorage" className="nav-link">
+              File Storage
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/chat" className="nav-link">
-            {language.Chat}
-          </Link>
-        </li>
+          <li>
+            <Link to="/chat" className="nav-link">
+              {language.Chat}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/private-chat" className="nav-link">
-            {language.Private_Messaging}
-          </Link>
-        </li>
+          <li>
+            <Link to="/private-chat" className="nav-link">
+              {language.Private_Messaging}
+            </Link>
+          </li>
 
-        <li className="nav-item">
-          <Link to="/todo-list" className="nav-link">
-            {language.Todo}
-          </Link>
-        </li>
+          <li>
+            <Link to="/todo-list" className="nav-link">
+              {language.Todo}
+            </Link>
+          </li>
 
-          <li className="nav-item">
+          <li>
             <Link to="#" onClick={Calendar} className="nav-link">
               Calendar
             </Link>
           </li>
-          
-          <li className="nav-item">
+
+          <li>
             <Link to="/forum" className="nav-link">
               Forum
             </Link>
           </li>
-        
-        <li className="nav-item">
-          <Link to="/userprofile" className="nav-link">
-            {language.User_Profile}
-          </Link>
-        </li>
 
-        <li className="nav-item dropdown">
+          <li>
+            <Link to="/userprofile" className="nav-link">
+              {language.User_Profile}
+            </Link>
+          </li>
+
+          <li>
+            <i className="fas fa-door-open sign-out-button" onClick={onLogout} />
+          </li>
+        </ul>
+        <div className="lang-drop">
           <div
             className="nav-link dropdown-toggle"
             href="#"
@@ -100,11 +104,10 @@ function Navbar({ onLogout }) {
               {language.swedish}
             </div>
           </div>
-        </li>
-      </ul>
-      <i className="fas fa-door-open sign-out-button" onClick={onLogout} />
-    </nav>
-);
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
