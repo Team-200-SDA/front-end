7ce767916b64d1dfa837d14e3d9cdf0827bd9ba2
Sneha <sneha.gupta@gmail.com>
2020-12-12 18:35:09 +0100

    Add login Page styling-basic

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -3,48 +3,44 @@ import { useContext } from 'react';
 import { LangContext } from '../../contexts/LanguageContext';
 
 function LoginForm({ onSubmit }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { language } = useContext(LangContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { language } = useContext(LangContext);
 
+  return (
+    <div className="card-body">
+      <h4 className="card-title">{language.login}</h4>
+      <div>
+        <div className="form-group">
+          <label>{language.email}:</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder={language.email}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
 
-    return (
-            <div className="card">
-            <div className="card-body loginregpage">
-                <h4 className="card-title" >{language.login}</h4>
-                <div>
-                    <div className="form-group">
-                        <label>{language.mail}</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            placeholder={language.email}
-                            value={email}
-                            onChange={e => setEmail(e.target.value)} />
-                    </div>
+        <div className="form-group">
+          <label>{language.pass}:</label>
+          <input
+            type="password"
+            placeholder={language.pass}
+            className="form-control"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
 
-                    <div className="form-group">
-                        <label>{language.pass}</label>
-                        <input
-                            type="password"
-                            placeholder={language.pass}
-                            className="form-control"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)} />
-                    </div>
-
-                    <div className="form-group">
-                        <button
-                            className="btn btn-info"
-                            onClick={() => onSubmit({ email, password })}>
-                            {language.login}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    
-    );
+        <div className="form-group">
+          <button className="btn loginbutton" onClick={() => onSubmit({ email, password })}>
+            {language.login}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm;
--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Auth from "../../services/Auth";
-import LoginForm from "./LoginForm";
-import RegisterForm from "./RegisterForm";
-import { useContext } from "react";
-import { LangContext } from "../../contexts/LanguageContext";
-import BootstrapSwitchButton from "bootstrap-switch-button-react";
-import { Card } from "@material-ui/core";
+import React from 'react';
+import Auth from '../../services/Auth';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+import { useContext } from 'react';
+import { LangContext } from '../../contexts/LanguageContext';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
+import { Card } from '@material-ui/core';
 
 function LoginPage() {
   const { language, changeLanguage } = useContext(LangContext);
-  const login = async (loginData) => {
+  const login = async loginData => {
     const loginSuccess = await Auth.login(loginData);
 
     if (!loginSuccess) {
@@ -17,7 +17,7 @@ function LoginPage() {
     }
   };
 
-  const register = async (registrationData) => {
+  const register = async registrationData => {
     const registerSuccess = await Auth.register(registrationData);
     if (!registerSuccess) {
       alert(language.Couldnt_register);
@@ -25,50 +25,26 @@ function LoginPage() {
   };
 
   return (
-    <div className="login-page">
-      <div className="card login-header">
-        <div className="row">
-          <div className="col name">
-            <h2>Edulane</h2>
-          </div>
-          <div className="col lang">
-            <BootstrapSwitchButton
-              checked={true}
-              onlabel="EN"
-              onstyle="danger"
-              offlabel="SV"
-              offstyle="success"
-              style="w-10 mx-3"
-              onChange={changeLanguage}
-            />
-          </div>
-        </div>
-      </div>
-      {/*  <div className="container logincont">
-        <div className="login-card">
-          <div className="row loginregforms">
-            <div className="col 12 strong-shadow">
-              <LoginForm onSubmit={login} />
-            </div>
-
-            <div className="col 12 mt-4">
-              <RegisterForm onSubmit={register} />
-            </div>
-          </div>
-        </div>
-      </div> */}
-
-      <div className="col-md-6">
-        <div className="row loginrow">
-          <div className="col-12  strong-shadow">
+    <div className="card login-wrap">
+      <div className="login-picture">
+        <div className="card-body login-form-div">
+          <h1 className="brandName">Edulane</h1>
+          <div className="loginForm">
             <LoginForm onSubmit={login} />
           </div>
-
-          <div className="col-12 mt-4">
+          <div className="registerForm">
             <RegisterForm onSubmit={register} />
           </div>
         </div>
       </div>
+      <footer className="login-footer">
+        <span className="footer-link">
+          <a>English</a>
+        </span>
+        <span className="footer-link">
+          <a>Swedish</a>
+        </span>
+      </footer>
     </div>
   );
 }
