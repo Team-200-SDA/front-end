4a2573c459f247ea20c455fc6921fce4356b34d3
pXius <fan.engelbrecht@gmail.com>
2020-12-13 16:32:45 +0100

    Todo List Responsive Design Updates

--- src/components/todo/TodoCreateComponent.js
+++ src/components/todo/TodoCreateComponent.js
@@ -1,74 +1,81 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import TodoApi from "../../api/TodoApi";
-import { useContext } from "react";
-import { LangContext } from "../../contexts/LanguageContext";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import TodoApi from '../../api/TodoApi';
+import { useContext } from 'react';
+import { LangContext } from '../../contexts/LanguageContext';
 
-import { Button } from "@material-ui/core";
-
-//styling
-import "../../css/todo/_todo.css";
+import { Button, FormLabel } from '@material-ui/core';
 
 //Using Functional components to Create a student todo list.
 function StudentCreateComponent() {
   const { language } = useContext(LangContext);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [dueDate, setDueDate] = useState('');
   const history = useHistory();
 
   //Creation of the Todolist
-  const createTodo = async (event) => {
+  const createTodo = async event => {
     event.preventDefault();
     const todo = { title, description, dueDate, complete: false };
     await TodoApi.createTodo(todo);
-    history.push("/todo-list");
+    history.push('/todo-list');
   };
 
   return (
-    
-    <form onSubmit={(event) => createTodo(event)}>
-      <div className="card-body mt-3">
-        <h4>{language.Create_Todo}</h4>
-        <label>{language.Title}</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="form-control"
-          placeholder={language.Enter_Title}
-        />
-        <span style={{ color: "red" }}>{null}</span>
-        <br />
-
-        <label>{language.Description}</label>
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="form-control"
-          placeholder={language.Enter_Description}
-        />
-        <span style={{ color: "red" }}>{null}</span>
-        <br />
-
-        <label>{language.Due_Date}</label>
-        <input
-          type="text"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-          className="form-control"
-          placeholder={language.Enter_due_date}
-        />
-        <span style={{ color: "red" }}>{null}</span>
-        <br />
-
-        <Button type="submit" variant="contained" color="primary">
-          {language.Save}
-        </Button>
+    <>
+      <div className="title-div">
+        <h1 className="page-title-text">
+          <i className="fas fa-tasks title-icon"></i>
+          {language.Todo_List}
+        </h1>
       </div>
-    </form>
-    // </div>
+
+      <div className="card-body create-todo-card">
+        <form onSubmit={event => createTodo(event)}>
+          <div className="creation-form create-todo-label">Create a new Todo</div>
+          <FormLabel className="form-label" component="legend">
+            {language.Title}
+          </FormLabel>
+          <input
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="form-control subject-input"
+            placeholder={language.Enter_Title}
+          />
+          <FormLabel className="form-label" component="legend">
+            {language.Description}
+          </FormLabel>
+          <input
+            type="text"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            className="form-control subject-input"
+            placeholder={language.Enter_Description}
+          />
+          <FormLabel className="form-label" component="legend">
+            {language.Due_Date}
+          </FormLabel>
+          <input
+            type="text"
+            value={dueDate}
+            onChange={e => setDueDate(e.target.value)}
+            className="form-control subject-input last-input-todo"
+            placeholder={language.Enter_due_date}
+          />
+
+          <Button
+            className="create-todo-button"
+            type="submit"
+            variant="contained"
+            color="primary">
+            {/* Change from Save to Create Todo */}
+            Create Todo
+          </Button>
+        </form>
+      </div>
+    </>
   );
 }
 
