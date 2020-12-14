5df8054f915c4499be6a447ea4113a9abdd3a72a
Radha <radhasr1986@gmail.com>
2020-12-14 12:33:38 +0100

    Comments added for User profile components and File Storage components

--- src/components/userprofile/UserProfile.jsx
+++ src/components/userprofile/UserProfile.jsx
@@ -4,48 +4,67 @@ import UserApi from "../../api/UserApi";
 import { useContext } from "react";
 import { LangContext } from "../../contexts/LanguageContext";
 
+/**
+ * This component calls Api to get logged in user details and updates address, phone no and profile picture
+ */
 function UserProfile() {
   const { language } = useContext(LangContext);
   const [user, setUser] = useState([]);
 
+  /**
+   * Calls Api function to get logged in user and sets as current user
+   */
   const getUser = () => {
     UserApi.getLoggedInUser().then((res) => {
       setUser(res.data);
     });
   };
 
+  /**
+   * When loading user profile page, calls Api function to get logged in user and sets as current user
+   */
   useEffect(() => {
     getUser();
   }, []);
 
+  /**
+   *
+   * @param {String} address
+   * Calls Api to update address of the current user and 
+   * if update successful alerts the user that address is updated
+   */
   function updatedAddress(address) {
-    console.log(address);
-
     UserApi.updateAddress(address)
       .then((res) => {
         alert(language.Address_Updated);
-        console.log(res);
       })
       .catch((err) => console.log(err));
   }
 
+  /**
+   *
+   * @param {String} phoneno
+   * Calls Api to update phoneno of the current user and 
+   * if update successful alerts the user that phoneno is updated
+   */
   function updatedPhoneno(phoneno) {
-    console.log("Phoneno", phoneno);
-
     UserApi.updatePhoneno(phoneno)
       .then((res) => {
         alert(language.Phoneno_Updated);
-        console.log(res);
       })
       .catch((err) => console.log(err));
   }
 
+  /**
+   * 
+   * @param {String} image 
+   * Calls Api to update profile picture url of the current user and 
+   * if update successful alerts the user that profile picture is updated
+   */
   function updatedPic(image) {
-    console.log(image);
     UserApi.updateProfilepic(image)
       .then((res) => {
         alert(language.Profile_picture_Updated);
-        console.log("RESPONSE", res);
         setUser(res.data);
       })
       .catch((err) => console.log(err));
@@ -55,7 +74,7 @@ function UserProfile() {
     <div>
       <UserProfileForm
         user={user}
-        onUpdateClick={updatedAddress}
+        onUpdateAddressClick={updatedAddress}
         onUpdatePhoneClick={updatedPhoneno}
         onUpdatePicClick={updatedPic}
       />
